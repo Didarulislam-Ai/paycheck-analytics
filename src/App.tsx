0950cf4e06f5d924d71b73969a0fe7ef41bf91d5
Didarulislam-Ai <73573028+Didarulislam-Ai@users.noreply.github.com>
2025-06-01 19:42:30 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CanadianPaycheckAnalytics from './components/paycheck_analytics_dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <CanadianPaycheckAnalytics />
+    </div>
+  );
+}
+
+export default App; 
